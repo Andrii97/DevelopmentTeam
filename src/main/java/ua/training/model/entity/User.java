ee0d1611ae06b9b7599c93b3f4d924bdef9d1a84
Andrii97 <Severinandrey97@gmail.com>
2017-01-19 20:26:54 +0200

    added builder

--- src/main/java/ua/training/model/entity/User.java
+++ src/main/java/ua/training/model/entity/User.java
@@ -11,7 +11,7 @@ public class User {
     private String email;
     private String password;
     private Role role;
-    private boolean isActive;
+    private boolean isActive = true;
 
     public Integer getId() {
         return id;
@@ -75,6 +75,98 @@ public class User {
 
     public void setActive(boolean active) {
         isActive = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (isActive != user.isActive) return false;
+        if (!id.equals(user.id)) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (middleName != null ? !middleName.equals(user.middleName) : user.middleName != null) return false;
+        if (!lastName.equals(user.lastName)) return false;
+        if (!email.equals(user.email)) return false;
+        if (!password.equals(user.password)) return false;
+        return role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (middleName != null ? middleName.hashCode() : 0);
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + role.hashCode();
+        result = 31 * result + (isActive ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+    public static class Builder{
+        User instance = new User();
+
+        public Builder setId(int id) {
+            instance.id = id;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            instance.firstName = firstName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            instance.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            instance.lastName = lastName;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            instance.email = email;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            instance.password = password;
+            return this;
+        }
+
+        public Builder setRole(Role role) {
+            instance.role = role;
+            return this;
+        }
+
+        public Builder setActive(boolean active) {
+            instance.isActive = active;
+            return this;
+        }
+
+        public User build() {
+            return instance;
+        }
     }
 
 }
